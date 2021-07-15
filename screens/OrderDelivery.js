8c371e248b111e7750ed8e0ef7ea1a3dfb79e8a4
stphncrt <cretestephen@gmail.com>
2021-07-16 01:51:47 +0200

    Basic Structure Formed

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const OrderDelivery = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export {OrderDelivery};
---
+++ screens/Resturant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Resturant = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export {Resturant};
